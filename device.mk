vdng9338/evolutionx_device_xiaomi_tulip

tulip: Enable power-libperfmgr
Change-Id: Ia5f8fb4a2afc7a453eb09d2092a7872fd8442bc9
Signed-off-by: Ratoriku <a1063021545@gmail.com>

device.mk
@@ -335,8 +335,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@2.0.vendor \
+    android.hardware.power-service.xiaomi_sdm660-libperfmgr \
     libqti-perfd-client
 
 PRODUCT_COPY_FILES += \

bittencourtjulival/tulip-ci

device.mk
@@ -370,7 +370,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power-service.tulip-libperfmgr \
     android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
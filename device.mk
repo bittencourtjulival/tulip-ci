eb2f0ae1747e1fa49d04fdc21452792ba1de1f99
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-01-21 12:25:17 +0530

    tulip: Enable power-libperfmgr
    Change-Id: Ia5f8fb4a2afc7a453eb09d2092a7872fd8442bc9
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- device.mk
+++ device.mk
@@ -370,7 +370,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power-service.tulip-libperfmgr \
     android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
