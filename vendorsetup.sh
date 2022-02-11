6a5b71e061879612068dc29bcccace5d6561158e
Julival Bittencourt <53379692+julival25@users.noreply.github.com>
2022-02-11 15:21:32 -0300

    Added vendorsetup again

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+rm -rf device/xiaomi/extras
+rm -rf vendor/opstuff/remove_packages
+rm -rf packages/resources/devicesettings
+rm -rf external/ant-wireless/antradio-library
+rm -rf device/qcom/sepolicy-legacy-um
